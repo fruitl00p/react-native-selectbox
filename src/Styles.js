6b450584248f70db2a88830f87158079c19a24c5
Robin Speekenbrink <robin@kingsquare.nl>
2017-05-08 10:09:47 +0200

    jscs cleanup

--- src/Styles.js
+++ src/Styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Dimensions } from 'react-native';
+
 const { height, width } = Dimensions.get('window');
 
 const PADDING = 8;
@@ -9,98 +10,98 @@ const OPTION_CONTAINER_HEIGHT = 400;
 
 export default StyleSheet.create({
 
-    overlayStyle: {
-        width: width,
-        height: height,
-        backgroundColor: 'rgba(0,0,0,0.7)'
-    },
+  overlayStyle: {
+    width,
+    height,
+    backgroundColor: 'rgba(0,0,0,0.7)'
+  },
 
-    optionContainer: {
-        borderRadius: BORDER_RADIUS,
-        width: width * 0.8,
-        height: OPTION_CONTAINER_HEIGHT,
-        backgroundColor: 'rgba(255,255,255,0.8)',
-        left: width * 0.1,
-        top: (height-OPTION_CONTAINER_HEIGHT)/2
-    },
+  optionContainer: {
+    borderRadius: BORDER_RADIUS,
+    width: width * 0.8,
+    height: OPTION_CONTAINER_HEIGHT,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    left: width * 0.1,
+    top: (height - OPTION_CONTAINER_HEIGHT) / 2
+  },
 
-    optionContainerInnerContainer: {
-        paddingHorizontal:10
-    },
+  optionContainerInnerContainer: {
+    paddingHorizontal: 10
+  },
 
-    promptContainer: {
-        left: width * 0.1,
-        top: (height - OPTION_CONTAINER_HEIGHT)/2 - 10
-    },
+  promptContainer: {
+    left: width * 0.1,
+    top: ((height - OPTION_CONTAINER_HEIGHT) / 2) - 10
+  },
 
-    cancelContainer: {
-        left: width * 0.1,
-        top: (height-OPTION_CONTAINER_HEIGHT)/2 + 10
-    },
+  cancelContainer: {
+    left: width * 0.1,
+    top: ((height - OPTION_CONTAINER_HEIGHT) / 2) + 10
+  },
 
-    selectStyle: {
-        flex: 1,
-        padding: 8,
-    },
+  selectStyle: {
+    flex: 1,
+    padding: 8,
+  },
 
-    selectContainerStyle: {
-        flex: 1,
-        flexDirection: "row",
-        alignItems:"center"
-    },
+  selectContainerStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
 
-    selectLabelStyle: {
-        flex: 1,
-        color: '#333',
-        fontSize: FONT_SIZE
-    },
+  selectLabelStyle: {
+    flex: 1,
+    color: '#333',
+    fontSize: FONT_SIZE
+  },
 
-    promptStyle: {
-        borderRadius: BORDER_RADIUS,
-        width: width * 0.8,
-        backgroundColor: 'rgba(255,255,255,0.8)',
-        padding: PADDING
-    },
+  promptStyle: {
+    borderRadius: BORDER_RADIUS,
+    width: width * 0.8,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    padding: PADDING
+  },
 
-    promptLabelStyle: {
-        textAlign: 'center',
-        color: '#333',
-        fontSize: FONT_SIZE
-    },
+  promptLabelStyle: {
+    textAlign: 'center',
+    color: '#333',
+    fontSize: FONT_SIZE
+  },
 
-    cancelStyle: {
-        borderRadius: BORDER_RADIUS,
-        width: width * 0.8,
-        backgroundColor: 'rgba(255,255,255,0.8)',
-        padding: PADDING
-    },
+  cancelStyle: {
+    borderRadius: BORDER_RADIUS,
+    width: width * 0.8,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    padding: PADDING
+  },
 
-    cancelLabelStyle: {
-        textAlign: 'center',
-        color: '#333',
-        fontSize: FONT_SIZE
-    },
+  cancelLabelStyle: {
+    textAlign: 'center',
+    color: '#333',
+    fontSize: FONT_SIZE
+  },
 
-    optionStyle: {
-        padding: PADDING,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
-    },
+  optionStyle: {
+    padding: PADDING,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
 
-    optionLabelStyle: {
-        textAlign: 'center',
-        fontSize: FONT_SIZE,
-        color: HIGHLIGHT_COLOR
-    },
+  optionLabelStyle: {
+    textAlign: 'center',
+    fontSize: FONT_SIZE,
+    color: HIGHLIGHT_COLOR
+  },
 
-    sectionStyle: {
-        padding: PADDING * 2,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
-    },
+  sectionStyle: {
+    padding: PADDING * 2,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
 
-    sectionLabelStyle: {
-        textAlign: 'center',
-        fontSize: FONT_SIZE
-    }
+  sectionLabelStyle: {
+    textAlign: 'center',
+    fontSize: FONT_SIZE
+  }
 });
